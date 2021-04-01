518be30545d4200bc2368e4cfc814b703817741a
Javier Cazalilla <al363268@uji.es>
2021-04-01 16:50:07 +0200

    api added to environment

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Movie } from '../models/movie.model';
 import { GenericResponse } from '../models/tmdbGenericResponse.model';
 
@@ -13,15 +14,17 @@ export class MoviesService {
     private http: HttpClient
   ) { }
 
+  api: string = environment.api;
+
   getMovies(){
-    return this.http.get<GenericResponse<Movie>>('https://api.themoviedb.org/3/discover/movie?api_key=9b06d6cb3a9bff49394c6dcd24a4ec19');
+    return this.http.get<GenericResponse<Movie>>(`${this.api}discover/movie?api_key=9b06d6cb3a9bff49394c6dcd24a4ec19`);
   }
   getMoviesLike(text: string){
-    return this.http.get<GenericResponse<Movie>>('https://api.themoviedb.org/3/search/movie?api_key=9b06d6cb3a9bff49394c6dcd24a4ec19&query='
+    return this.http.get<GenericResponse<Movie>>(`${this.api}search/movie?api_key=9b06d6cb3a9bff49394c6dcd24a4ec19&query=`
      + text.replace(' ', '+')
     );
   }
   getMovieById(id: string){
-    return this.http.get<Movie>('https://api.themoviedb.org/3/movie/' + id + '?api_key=9b06d6cb3a9bff49394c6dcd24a4ec19');
+    return this.http.get<Movie>(`${this.api}movie/` + id + `?api_key=9b06d6cb3a9bff49394c6dcd24a4ec19`);
   }
 }
